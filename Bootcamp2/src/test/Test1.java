85233e5324ac5dc5ad28633d7aeb494ee6c63c2d
Zoran Huliselan <CloseSure@MacBook-Air-van-Zoran-420.local>
2019-11-12 19:13:48 +0100

    mavenproject

---
+++ Bootcamp2/src/test/Test1.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	@Test
+		void test()	{
+			AssertTrue(true)
+			
+			@Test
+			void test()	{
+				AssertTrue(true)
+					
+				
+			
+		}
+	}
+
+	}
+
+}
